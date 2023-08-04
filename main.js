be71b44add60db453ae7d6bec7905dc2a40c7427
Kashish <kashmehrotra2003@gmail.com>
2023-08-05 00:18:03 +0530

    Project added

---
+++ main.js
@@ -0,0 +1,7 @@
+const searchbtn = document.getElementById('search-btn')
+const searchabar= document.querySelector('.search-bar-container')
+
+
+searchbtn.addEventListener("click",  { } => {
+    searchbtn.classList.toggle{"fa-times"}
+})
